02b99db2605490e06ff937683ea6874b6fbe4163
twopats <lpatipat@icloud.com>
2024-02-04 04:56:03 -0500

    adding new pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				libre: ["Roboto", ...defaultTheme.fontFamily.sans]
+			},
+			colors: {
+				'primary': '#3F5B3C',
+				'secondary': '#4EA595',
+				'dark': '#253636'
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
 }
